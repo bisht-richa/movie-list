e0f6710e8b167875810fa1eda7fb35cf39afe3ea
bisht-richa <richa.bisht@suse.com>
2020-12-01 20:18:09 +0100

    Search fixed

---
+++ api/routes/movies.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios')
+
+require('dotenv').config()
+const api_keys = process.env.API_KEY
+
+const data = async () => {
+    const urlData = 'https://www.omdbapi.com/?s=city&apikey=e730671f'
+    const request = await axios.get(urlData)
+    const response = await request.data
+    return response
+}
+
+const validateKey = (req, res, next) => {
+  let api_key = req.headers['x-api-key']
+  if (api_key == api_keys ) {
+      next()
+  } else {
+      res.status(403).send({ error: { code: 403, message: 'You not allowed.' } });
+  }
+}
+
+router.get('/', validateKey, (req, res, next) => {
+  data()
+  .then(result => {
+    res.status(200).send(result)
+  })
+})
+
+router.get('/search', (req, res, next) => {
+  const searchQuery = req.query.q
+  var searchResult = []
+
+  if (searchQuery) {
+    data()
+    .then(result => {
+    var searchResult = result.Search.filter((ele) => {
+      if (ele.Title.toLowerCase().includes(searchQuery.toLowerCase())){
+          return ele
+      }
+    })
+      res.status(200).send(searchResult)
+    })
+  } else {
+    res.status(200).send("Null")
+  }
+})
+
+module.exports = router;
--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const bodyParser = require('body-parser')
 const cors = require('cors');
 const app = express();
 
-const productsRoute = require('./api/routes/products')
-
+const moviesRoute = require('./api/routes/movies')
+    
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.use(cors())
 
-app.use('/products', productsRoute)
+app.use('/', moviesRoute)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
