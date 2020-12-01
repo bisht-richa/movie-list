607bd759b638d97592e883f89bf389c7943d78d1
bisht-richa <richa.bisht@suse.com>
2020-12-01 21:22:57 +0100

    modified server

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const http = require('http');
 const app = require('./app')
 
 const port = process.env.PORT || 5000;
-
 const server = http.createServer(app)
 
 server.listen(port, () => console.log(`BACK_END_SERVICE_PORT: ${port}`));
